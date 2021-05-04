f830912b3b30a6a520aa1e68945c9584a47e2254
Michelle Watts <watts.e.michelle@gmail.com>
2021-05-04 22:03:12 +1000

    follow readme generator write file async

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const generateMarkdown = require("./lib/generateMarkdown")
+const util = require('util');
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+// run inquirer questions
+const promptUser = () => {
+    return inquirer.prompt([{
+        type: 'list',
+        name: 'role',
+        message: 'Who would you like to make a profile for?',
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern"
+        ]
+      },
+     
+    ]);
+  };
+  
+  
+  
+  // creates a readme
+  const init = () => {
+    promptUser()
+      .then((answers) => writeFileAsync('./dist/index.html', generateMarkdown(answers)))
+      .then(() => {
+        console.log('Successfully wrote a index.html page');
+  
+      })
+      .catch((err) => console.error(err));
+  
+  
+  };
+  
+  // runs the init function
+  init();
