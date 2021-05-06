b40cb47e1a7e437a1628d34aade26a4289666052
Michelle Watts <watts.e.michelle@gmail.com>
2021-05-06 11:02:13 +1000

    added intern

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const generateMarkdown = require('./lib/generateMarkdown')
 // where all the team members get stored
 let teamMembers = [];
 
+
+
 // ask the inital questions about the team manager
 const promptUser = () => {
     return inquirer
@@ -40,14 +42,13 @@ const promptUser = () => {
 
             },
         ])
-// make a constructor object from the subclass Manager
+        // make a constructor object from the subclass Manager
         .then(function (answer) {
             const name = answer.name
             const email = answer.email
             const id = answer.id
             const git = answer.git
-            const role = "Manager"
-            const teamMember = new Manager(role, name, id, email, git)
+            const teamMember = new Manager(name, id, email, git)
             teamMembers.push(teamMember)
             moreTeamMembers();
         });
@@ -81,13 +82,53 @@ const addEngineer = () => {
 
             },
         ])
-// make a constructor object from the subclass Manager
+        // make a constructor object from the subclass Manager
         .then(function (answer) {
             const name = answer.name
             const email = answer.email
             const id = answer.id
             const git = answer.git
             const teamMember = new Engineer(name, id, email, git)
+            teamMembers.push(teamMember)
+            moreTeamMembers();
+        });
+
+}
+
+const addIntern = () => {
+    return inquirer
+        .prompt([{
+                type: 'input',
+                name: 'name',
+                message: 'What is the Intern`s name?',
+
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is their email?',
+
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is their ID number?',
+
+            },
+            {
+                type: 'input',
+                name: 'git',
+                message: 'What is their GitHub?',
+
+            },
+        ])
+        // make a constructor object from the subclass Manager
+        .then(function (answer) {
+            const name = answer.name
+            const email = answer.email
+            const id = answer.id
+            const git = answer.git
+            const teamMember = new Intern(name, id, email, git)
             teamMembers.push(teamMember)
             moreTeamMembers();
         });
@@ -104,14 +145,14 @@ const moreTeamMembers = () => {
             default: true,
         }, ])
 
-// when the user is finished adding team members this runs
+        // when the user is finished adding team members this runs
         .then((answer) => {
             if (answer.moreMembers === true) {
                 return addMore();
             } else
                 mapMembersCards();
-                console.log(mapMembersCards);
-                writeFileAsync('index.html', generateMarkdown(mapMembersCards(teamMembers)))
+            console.log(mapMembersCards);
+            writeFileAsync('index.html', generateMarkdown(mapMembersCards(teamMembers)))
             console.log('Successfully wrote a index.html');
         })
         .catch((err) => console.error(err));
@@ -120,47 +161,42 @@ const moreTeamMembers = () => {
 
 // runs the next function for who they want to add to their project
 const addMore = () => {
-    // console.log("You have said yes");
-    // console.log(teamMembers)
-    // promptUser();
     return inquirer
-    .prompt([{
-        type: 'list',
-        name: 'addMemberType',
-        message: 'Which type team member would you like to add?',
-        choices: ["Engineer", "Intern"],
-    }, ])
-    .then((answer) => {
-        if (answer.addMemberType === "Engineer") {
-            return addEngineer();
-        } else
-            mapMembersCards();
+        .prompt([{
+            type: 'list',
+            name: 'addMemberType',
+            message: 'Which type team member would you like to add?',
+            choices: ["Engineer", "Intern"],
+        }, ])
+        .then((answer) => {
+            if (answer.addMemberType === "Engineer") {
+                return addEngineer();
+            } else if (answer.addMemberType === "Intern") {
+                return addIntern();
+            } else
+                mapMembersCards();
             console.log(mapMembersCards);
             writeFileAsync('index.html', generateMarkdown(mapMembersCards(teamMembers)))
-        console.log('Successfully wrote a index.html');
-    })
-    .catch((err) => console.error(err));
+            console.log('Successfully wrote a index.html');
+        })
+        .catch((err) => console.error(err));
 };
-
-
-
 
 
 // runs just before the file is written to map each object to this html snippit
 const mapMembersCards = () => {
     console.log(teamMembers);
     const mapCard = teamMembers.map(function (data) {
-            return `<div class="card">   
+        return `<div class="card">   
         <div class="card-body">
-          <h5 class="card-title">${data.name}</h5>
-        
-          <p class="card-text">${data.getRole()}</p>
-          <p class="card-text">${data.id}</p>
-          <p class="card-text">${data.email}</p>
-          <p class="card-text">${data.github}</p>
+          <h5 class="card-title">Name: ${data.name}</h5>
+          <p class="card-text">Role: ${data.getRole()}</p>
+          <p class="card-text">ID: ${data.id}</p>
+          <p class="card-text">Email: <a href="mailto:${data.email}">${data.email}</a></p>
+          <p class="card-text">GitHub: <a href="${data.github}">${data.github}</a></p>
         </div>
       </div>`
-       
+
     });
 
     return mapCard.join("")
